98b88f8f2d80516daed5f912299fe763428414a4
2stinkysocks <zmehall@gmail.com>
2021-10-08 09:13:07 -0700

    add multithreading and update version

--- src/main/java/com/twostinkysocks/activitychecker/ActivityCommand.java
+++ src/main/java/com/twostinkysocks/activitychecker/ActivityCommand.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonParser;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+import scala.tools.nsc.Global;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,84 +15,100 @@ import java.net.URL;
 public class ActivityCommand extends SimpleCommand.ProcessCommandRunnable {
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
-        if(args != null && args.length <= 0) {
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Usage: /activity <player>"));
-        } else {
-            try {
-                URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + args[0]);
+        new Thread(new ActivityRunnable(sender, args)).start();
+    }
 
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    public class ActivityRunnable implements Runnable {
 
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("accept", "application/json");
+        private ICommandSender sender;
+        private String[] args;
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        public ActivityRunnable(ICommandSender sender, String[] args) {
+            this.sender = sender;
+            this.args = args;
+        }
 
-                if(connection.getResponseCode() != 200) {
-                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Invalid username. Usage: /activity <player>"));
-                } else {
-                    StringBuilder response = new StringBuilder("");
-                    String inputLine;
-                    while((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    connection.disconnect();
-                    JsonParser parser = new JsonParser();
-                    JsonObject element = (JsonObject) parser.parse(response.toString());
-                    String uuid = element.get("id").getAsString();
+        @Override
+        public void run() {
+            if(args != null && args.length <= 0) {
+                sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Usage: /activity <player>"));
+            } else {
+                try {
+                    URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + args[0]);
 
-                    // hypixel api
-                    if(ActivityChecker.getConfig().get("api", "key", "" ) == null) {
-                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Invalid request! (Try running /api new)"));
-                        return;
-                    }
-                    url = new URL("https://api.hypixel.net/status?uuid=" + uuid + "&key=" + ActivityChecker.getConfig().get("api", "key", "").getString());
-                    connection = (HttpURLConnection) url.openConnection();
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
                     connection.setRequestMethod("GET");
                     connection.setRequestProperty("accept", "application/json");
 
-                    in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
                     if(connection.getResponseCode() != 200) {
-                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Invalid request! (Try running /api new)"));
+                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Invalid username. Usage: /activity <player>"));
                     } else {
-                        response = new StringBuilder("");
+                        StringBuilder response = new StringBuilder("");
+                        String inputLine;
                         while((inputLine = in.readLine()) != null) {
                             response.append(inputLine);
                         }
                         connection.disconnect();
+                        JsonParser parser = new JsonParser();
+                        JsonObject element = (JsonObject) parser.parse(response.toString());
+                        String uuid = element.get("id").getAsString();
 
-                        parser = new JsonParser();
-                        element = (JsonObject) parser.parse(response.toString());
-                        boolean online = element.getAsJsonObject("session").get("online").getAsBoolean();
-                        String gameType = "";
-                        String mode = "";
-                        String map = "";
-                        if(online) {
-                            if(element.getAsJsonObject("session").get("gameType") != null) gameType = element.getAsJsonObject("session").get("gameType").getAsString();
-                            if(element.getAsJsonObject("session").get("mode") != null) mode = element.getAsJsonObject("session").get("mode").getAsString();
-                            if(element.getAsJsonObject("session").get("map") != null) map = element.getAsJsonObject("session").get("map").getAsString();
+                        // hypixel api
+                        if(ActivityChecker.getConfig().get("api", "key", "" ) == null) {
+                            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Invalid request! (Try running /api new)"));
+                            return;
+                        }
+                        url = new URL("https://api.hypixel.net/status?uuid=" + uuid + "&key=" + ActivityChecker.getConfig().get("api", "key", "").getString());
+                        connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.setRequestProperty("accept", "application/json");
+
+                        in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                        if(connection.getResponseCode() != 200) {
+                            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Invalid request! (Try running /api new)"));
+                        } else {
+                            response = new StringBuilder("");
+                            while((inputLine = in.readLine()) != null) {
+                                response.append(inputLine);
+                            }
+                            connection.disconnect();
+
+                            parser = new JsonParser();
+                            element = (JsonObject) parser.parse(response.toString());
+                            boolean online = element.getAsJsonObject("session").get("online").getAsBoolean();
+                            String gameType = "";
+                            String mode = "";
+                            String map = "";
+                            if(online) {
+                                if(element.getAsJsonObject("session").get("gameType") != null) gameType = element.getAsJsonObject("session").get("gameType").getAsString();
+                                if(element.getAsJsonObject("session").get("mode") != null) mode = element.getAsJsonObject("session").get("mode").getAsString();
+                                if(element.getAsJsonObject("session").get("map") != null) map = element.getAsJsonObject("session").get("map").getAsString();
+                            }
+
+                            StringBuilder message = new StringBuilder("");
+                            message.append(EnumChatFormatting.GREEN + "---------------------------------\n\n");
+                            message.append("  " + args[0] + " is " + (online ? EnumChatFormatting.GREEN + "" + EnumChatFormatting.BOLD + "ONLINE\n\n" :  EnumChatFormatting.RED + "" + EnumChatFormatting.BOLD + "OFFLINE\n\n"));
+                            if(online) {
+                                message.append(EnumChatFormatting.AQUA + "  Playing: " + gameType + "\n\n");
+                                message.append(EnumChatFormatting.AQUA + "  Mode: " + mode + "\n\n");
+                                message.append(EnumChatFormatting.AQUA + "  Map: " + map + "\n\n");
+                            }
+                            message.append(EnumChatFormatting.GREEN + "---------------------------------");
+                            sender.addChatMessage(new ChatComponentText(message.toString()));
                         }
 
-                        StringBuilder message = new StringBuilder("");
-                        message.append(EnumChatFormatting.GREEN + "---------------------------------\n\n");
-                        message.append("  " + args[0] + " is " + (online ? EnumChatFormatting.GREEN + "" + EnumChatFormatting.BOLD + "ONLINE\n\n" :  EnumChatFormatting.RED + "" + EnumChatFormatting.BOLD + "OFFLINE\n\n"));
-                        if(online) {
-                            message.append(EnumChatFormatting.AQUA + "  Playing: " + gameType + "\n\n");
-                            message.append(EnumChatFormatting.AQUA + "  Mode: " + mode + "\n\n");
-                            message.append(EnumChatFormatting.AQUA + "  Map: " + map + "\n\n");
-                        }
-                        message.append(EnumChatFormatting.GREEN + "---------------------------------");
-                        sender.addChatMessage(new ChatComponentText(message.toString()));
                     }
-
+                } catch(Exception e) {
+                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "An unexpected error has occured."));
+                    sender.addChatMessage(new ChatComponentText(e.getMessage()));
+                    sender.addChatMessage(new ChatComponentText(e.getStackTrace()[0].toString()));
                 }
-            } catch(Exception e) {
-                sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "An unexpected error has occured."));
-                sender.addChatMessage(new ChatComponentText(e.getMessage()));
-                sender.addChatMessage(new ChatComponentText(e.getStackTrace()[0].toString()));
-            }
 
+            }
         }
     }
 }
